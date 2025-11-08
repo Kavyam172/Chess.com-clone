38660072464397cef4e13935c4d626f028748edc
KavyamSachdeva <124245507+Kavyam172@users.noreply.github.com>
2025-11-08 22:33:44 +0530

    update WebSocket URL to use VITE_BACKEND_URL instead of VITE_WS_URL

--- frontend/frontend/src/hooks/useSocket.ts
+++ frontend/frontend/src/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const WS_URL = import.meta.env.ENV==='development'? "ws://localhost:8080" : import.meta.env.VITE_WS_URL
+const WS_URL = import.meta.env.ENV==='development'? "ws://localhost:8080" : import.meta.env.VITE_BACKEND_URL
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
